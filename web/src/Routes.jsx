b68fddce7d86d5fcead64b61013ef59f294a43d2
mxgc <mx@great.cat>
2023-08-15 11:26:52 -0700

    cp1 complete

--- web/src/Routes.jsx
+++ web/src/Routes.jsx
@@ -7,12 +7,18 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Set } from '@redwoodjs/router'
+
+import BlogLayout from 'src/layouts//BlogLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route notfound page={NotFoundPage} />
+      <Set wrap={BlogLayout}>
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
+        <Route notfound page={NotFoundPage} />
+      </Set>
     </Router>
   )
 }
---
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,13 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>This site was created to demonstrate my mastery of Redwood again!</p>
+    </>
+  )
+}
+
+export default AboutPage
